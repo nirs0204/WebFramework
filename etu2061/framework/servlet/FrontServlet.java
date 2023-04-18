77c556bfe27dec7c922788f45c583b052830eb31
nirs0204 <118588963+nirs0204@users.noreply.github.com>
2023-04-18 06:01:50 +0300

    Le sprint 1

---
+++ etu2061/framework/servlet/FrontServlet.java
@@ -0,0 +1,41 @@
+package etu2061.framework.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+import java.io.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.*;
+
+
+public class FrontServlet extends HttpServlet{
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            PrintWriter out = response.getWriter();
+            String requestUrl = request.getRequestURL().toString();
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Ma Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Bienvenue sur ma Servlet!</h1>");
+            out.println("<p>URL de la requete: " + requestUrl + "</p>");
+            out.println("</html>");
+            out.println("</body>");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
